3976aa6990c848a98d726ab2a983580833cc9c7f
hls93 <hlshaw3@ncsu.edu>
2017-08-09 15:42:06 -0400

    fixing timestamps

--- todos.sql
+++ todos.sql
@@ -1,25 +1,25 @@
 create database todoList;
-create table todos (id integer, title text not null, details text, priority integer not null, created_at integer not null, completed_at integer);
+create table todos (id integer, title varchar not null, details varchar, priority integer not null, created_at timestamp not null, completed_at timestamp);
 
-INSERT INTO todos (id, title, details, priority, created_at,completed_at)
-VALUES (1, 'Laundry', 'wash and dry', 1, 10/07, 10/08),
-       (2, 'Dishes', 'wash and dry', 3, 10/07)
-       (3, 'Vacuum', 'carpets and tile', 1, 10/08, 10/08)
-       (4, 'Make bed', 4, 10/09, 10/12)
-       (5, 'Mop', 'kitchen', 1, 10/07);
+INSERT INTO todos (id, title, details, priority, created_at, completed_at)
+VALUES ('Laundry', 'wash and dry', 1, now(), 10/08),
+       ('Dishes', 'wash and dry', 3, now())
+       ('Vacuum', 'carpets and tile', 1, now(), 10/08)
+       ('Make bed', 4, 10/09, now())
+       ('Mop', 'kitchen', 1, now());
 
-SELECT  completed_at
+SELECT  *
 FROM todos
-WHERE completed_at = null;
+WHERE completed_at IS null;
 
-SELECT priority
+SELECT *
 FROM todos
 WHERE priority > 1;
 
-UPDATE todos, 2
-SET  completed_at = null
-WHERE price = 10/08;
+UPDATE todos
+SET  completed_at = timestamp now()
+WHERE id = 2;
 
 DELETE
 FROM todos
-WHERE completed_at = not null;
+WHERE completed_at IS not null;
